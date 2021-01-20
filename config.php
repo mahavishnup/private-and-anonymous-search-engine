bc020fc76cd394562eae8dde5489e91c772046a7
Mahavishnu P <52984889+mahavishnup@users.noreply.github.com>
2021-01-20 07:59:29 +0530

    config edit

--- config.php
+++ config.php
@@ -2,7 +2,8 @@
 ob_start();
 
 try {
-	$con = new PDO("mysql:dbname=moodle;host=localhost", "root", "");
+	$con = new PDO("mysql:dbname=heroku_2becae845a23420;host=us-cdbr-east-02.cleardb.com", "b0cd4d1d1ae86e", "46ed9a39");
+	// $con = new PDO("mysql:dbname=moodle;host=localhost", "root", "");
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch(PDOExeption $e) {
